6efea5f740fad46648b596e69f54c964488269c0
PedroBarge <pedro525tavares@gmail.com>
2023-11-24 11:58:02 +0000

    mw2 done
    need to add numbers

--- MinesWeeper/src/Main.java
+++ MinesWeeper/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Grid grid = new Grid();
+        grid.makeNewGrid();
+        grid.buildGrid();
+
+        placeBombsOnGrid placeBombsOnGrid = new placeBombsOnGrid();
+        placeBombsOnGrid.plceBombs();
+
+        CheckBombs.playerTryToFindBoms();
     }
 }
